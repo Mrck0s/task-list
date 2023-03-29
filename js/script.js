eed836752959d23cf4cb34cee493a2d40c0f2311
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-03-29 14:54:57 -0300

    cambios simple y js
    algunos cambios en index y css. constantes de js creadas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Constantes para añadir la tarea a la lista
+const tareaEntrada = document.querySelector("#input-texto");
+const btnAdd = document.querySelector("#input-submit");
+const taskContainer = document.querySelector("#tareas-container");
+const tarea = document.querySelector("tarea");
+
+//Constantes de la propia tarea
+const btnCheck = document.querySelector("#input-text");
+const btnBorrar = document.querySelector("#input-eliminar");
+const tareaTexto = document.querySelector("#texto-tarea");
