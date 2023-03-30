2fc0ddcad6f5ff1ade55348a0dd5d234ac8116f7
Mrck0s <110942627+Mrck0s@users.noreply.github.com>
2023-03-30 19:24:25 -0300

    terminado
    codigo terminado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
 // Constantes para añadir la tarea a la lista
 const tareaEntrada = document.querySelector("#input-texto");
 const btnAdd = document.querySelector("#input-submit");
-const taskContainer = document.querySelector("#tareas-container");
-const tarea = document.querySelector("tarea");
+const listaTareas = document.querySelector(".tareas-lista");
+const vacio = document.querySelector(".vacio");
 
-//Constantes de la propia tarea
-const btnCheck = document.querySelector("#input-text");
-const btnBorrar = document.querySelector("#input-eliminar");
-const tareaTexto = document.querySelector("#texto-tarea");
+// Función para añadir la tarea a la lista
+btnAdd.addEventListener("click", (e) => {
+    e.preventDefault();
+    const tareaTexto = tareaEntrada.value;
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    if (tareaTexto.value != "") {
+        li.classList.add("tarea");
+    li.classList.add("borde-radio");
+    p.classList.add("texto-tarea");
+    p.textContent = tareaTexto;
+    li.appendChild(p);
+    li.appendChild(addBtnDelete());
+    listaTareas.appendChild(li);
+    tareaEntrada.value = "";
+    vacio.style.display = "none";
+    } else {
+        alert("Escribe la tarea que deseas añadir");
+    }
+    tareaEntrada.focus();
+})
+
+// Funcion que agrega el boton de borrado a la tarea
+function addBtnDelete () {
+    const btnDelete = document.createElement("button");
+    btnDelete.classList.add("input-tarea");
+    btnDelete.classList.add("borde-radio");
+    btnDelete.classList.add("input-eliminar");
+    btnDelete.innerText = "x";
+
+    btnDelete.addEventListener("click", (e) => {
+        e.preventDefault();
+        const li = e.target.parentElement;
+        listaTareas.removeChild(li)
+
+        const items = document.querySelectorAll("li");
+        if (items.length < 1) {
+            vacio.style.display = "block";
+        } 
+    })
+    return btnDelete
+}
